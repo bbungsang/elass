3abab2b865aff1b117edc47d6130036edf9b6afa
bbungsang <821heejin@naver.com>
2017-09-04 16:43:45 +0900

    add Dockerfile, wsgi 파일 추가후 debug 모드 runserver

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -13,7 +13,7 @@ import json
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 ROOT_DIR = os.path.join(BASE_DIR)
 
